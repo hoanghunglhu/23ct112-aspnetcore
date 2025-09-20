2741675bc09e82c14b32989b6a3e17482cd13071
Nguyễn Ngọc Quang Phúc <nguyenphuc123bh1@gmail.com>
2025-09-20 10:15:46 +0700

    upload code

--- Entity/AppDbContext.cs
+++ Entity/AppDbContext.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace LearnApiNetCore.Entity;
-
-public class AppDbContext : DbContext
+namespace LearnAspNetCore.Entity
 {
-  public AppDbContext(DbContextOptions<AppDbContext> options)
-  : base(options) { }
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-  public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using LearnAspNetCore.Entity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add DbContext with SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+
+builder.Services.AddControllers();
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Đăng ký Controllers
-builder.Services.AddControllers();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,8 +25,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-// Map tới Controllers
 app.MapControllers();
 
 app.Run();
