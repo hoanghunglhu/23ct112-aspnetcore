504011fd5f565caa2af36d77cd8ff7a7d8b9cf90
Trần Đại Nguyên <trannguyent2502@gmail.com>
2025-09-20 10:16:04 +0700

    .upload pacakage

--- Program.cs
+++ Program.cs
@@ -9,11 +9,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
