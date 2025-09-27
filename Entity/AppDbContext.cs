87a417ec1c447c3250cb7e335bcf64b6c00c04f2
Cuongne2406 <cainnguyen2005@gmail.com>
2025-09-27 10:43:11 +0700

    update

--- Entity/AppDbContext.cs
+++ Entity/AppDbContext.cs
@@ -4,8 +4,8 @@ namespace LearnApiNetCore.Entity;
 
 public class AppDbContext : DbContext
 {
-  public AppDbContext(DbContextOptions<AppDbContext> options)
-  : base(options) { }
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+    : base(options) { }
 
-  public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; set; }
 }
