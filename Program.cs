cfcc11e240892ba853afbba33c3ec59ac6a14feb
Trần Đình Bắc <tdbac5002@gmail.com>
2025-11-06 08:29:38 +0000

    bai tap

--- Program.cs
+++ Program.cs
@@ -1,27 +1,40 @@
-
-
 using Microsoft.EntityFrameworkCore;
-
 using LearnApiNetCore.Entity;
-
+using LearnApiNetCore.Services;
+using Serilog;
 var builder = WebApplication.CreateBuilder(args);
-
-// Add DbContext with SQL Server
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+    builder.Host.UseSerilog();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddMemoryCache();
+builder.Services.AddScoped<EmailService>();
 var app = builder.Build();
 
 if(app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.MapControllers();
 
-app.Run();
-
+try
+{
+    Log.Information("Ứng dụng đang khởi động...");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Ứng dụng gặp lỗi nghiêm trọng và phải dừng lại!");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
