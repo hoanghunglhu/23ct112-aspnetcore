b62e9abcf54ec0bdacef4abd4380d3c35cc91b85
vhplphilong2005-star <vhplphilong2005@gmail.com>
2025-11-08 07:44:13 +0700

    Create email controllers.cs

---
+++ Controllers/email/email controllers.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using LearnApiNetCore.Services;
+
+namespace LearnApiNetCore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailController : ControllerBase
+    {
+        private readonly EmailService _emailService;
+
+        public EmailController(EmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendEmail([FromQuery] string to)
+        {
+            string subject = "Good luck!";
+            string body = "<h3>Xin chào!</h3><p>Tôi là vo huynh phi long</p>";
+
+            bool result = await _emailService.SendEmailAsync(to, subject, body);
+
+            if (result)
+                return Ok("Đã gửi email thành công!");
+            else
+                return StatusCode(500, "Gửi email thất bại!");
+        }
+    }
+}
