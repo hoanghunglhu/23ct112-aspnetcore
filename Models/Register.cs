db6d46e1e931553172ce38bc3585469e20385af9
Đoàn Văn Phúc <phucvandoan123@gmail.com>
2025-11-15 08:49:47 +0700

    update

--- Models/Register.cs
+++
@@ -1,8 +0,0 @@
-namespace LearnApiNetCore.Models
-{
-    public class LoginModel
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
