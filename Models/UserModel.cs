312401993072c7ba33d8053cc6ed7d7f461df834
Đoàn Văn Phúc <phucvandoan123@gmail.com>
2025-11-15 09:32:13 +0700

    update

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -3,6 +3,8 @@ namespace LearnApiNetCore.Models
     public class UserModel
     {
         public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
 
         public string name { get; set; }
 
