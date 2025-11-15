c2ca4468d44ad44da388a2dbbdf638e4f454766f
Tran Dinh Bac <tranbac2635@gmail.com>
2025-11-15 08:47:00 +0700

    11_15_2025

--- Program.cs
+++ Program.cs
@@ -19,11 +19,12 @@ var app = builder.Build();
 
 if(app.Environment.IsDevelopment())
 {
-     app.UseSwagger();
+    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.MapControllers();
+app.UseFileServer();
 
 try
 {
